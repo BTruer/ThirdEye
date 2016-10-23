89d445155f06c95586b6910cecd20cfe3c764131
BTruer <tarparab@outlook.com>
2016-10-23 09:51:58 -0400

    lindode pre

--- TEServer/app.py
+++ TEServer/app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO,send
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
-@socketio.on('works'):
-def handleMessage(msg):
-    print "message: " + msg
-    send(msg,broadcast=True)
+angle = 0
+
+@app.route('/')
+def index():
+    return "Go to http://[hostname]/motor/api/  to get it."
+
+@app.route('/motor/api/', methods=['GET'])
+def getIt():
+    return jsonify({'angle': angle})
+
+@app.route('/motor/api/<string:vrangle>', methods=['POST'])
+def updateIt(vrangle):
+    angle = int(vrangle[7:])
+    return jsonify({'angle': angle})
+
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(debug=True)
