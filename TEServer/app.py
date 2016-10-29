89d5284034e209f67fc4d7ae5c733e47cb838016
BTruer <tarparab@outlook.com>
2016-10-28 21:56:02 -0400

    Fixed the main api server

--- TEServer/app.py
+++ TEServer/app.py
@@ -1,22 +1,21 @@
-from flask import Flask,jsonify,request
+from flask import Flask,request
 
 app = Flask(__name__)
 
-angle = 0
+angle = "00000000"
 
 @app.route('/')
 def index():
     return "Go to http://[hostname]/motor/api/  to get it."
 
 @app.route('/motor/api/', methods=['GET'])
-def getIt():
-    return jsonify({'angle': angle})
+def getIt(angle):
+        return angle
 
 @app.route('/motor/api/<string:vrangle>', methods=['POST'])
-def updateIt(vrangle):
-    angle = int(vrangle[7:])
-    return jsonify({'angle': angle})
-
+def updateIt(vrangle,angle):
+    angle = vrangle
+    return angle
 
 if __name__ == '__main__':
     app.run(debug=True)
